057f0aaa7e49203c6658d549d1470c377c9fad52
Saurabh Sharma <37309661+saurabhshar@users.noreply.github.com>
2023-02-19 21:06:38 +0000

    remove tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Saurabh\'s Notes',
-  tagline: 'My Learning Notes',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <div className={styles.buttons}  style="display:flex;justify-content:center;align-items:center;text-align:left;margin-left:60px">
           📐 Software Architecture <br/>
           👨‍💻 Engineering Leadership <br/>
           👨‍🎨 DevOps
